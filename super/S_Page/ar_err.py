9e8fd48806d130a434dc012a7e712d6cecb1e770
ibrahem <ibrahem.al-radaei@outlook.com>
2025-05-08 23:40:50 +0300

    .

--- super/S_Page/ar_err.py
+++ super/S_Page/ar_err.py
@@ -10,9 +10,8 @@ if find_edit_error(old, new): return
 def find_edit_error(old, new):
     # Define the dictionary of conversion phrases
     conversion_phrases = {
-        "#تحويل [["
+        "#تحويل [[",
     }
-
     for phrase in conversion_phrases:
         if phrase in old and phrase not in new:
             print(f"ar_err.py found ({phrase}) in old but not in new. return True")
