911ff72fdf080cca2e5e378ddfeab960f461abd7
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-05-31 05:28:10 +0300

    Set BOTNAME environment variable based on script location

--- super/__init__.py
+++ super/__init__.py
@@ -1,2 +1,11 @@
 # -*- coding: utf-8 -*-
+import os
+if not os.getenv("BOTNAME"):
+    botname = "newapi"
 
+    if __file__.find("wikiapi_new") != -1:
+        botname = "wikiapi_new"
+
+    os.environ["BOTNAME"] = botname
+
+    # os.getenv("BOTNAME")
