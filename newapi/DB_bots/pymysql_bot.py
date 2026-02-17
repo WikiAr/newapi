584bdd7005b4a6a30bbddbe4c7d3166ade927831
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-17 04:04:03 +0300

    .

--- newapi/DB_bots/pymysql_bot.py
+++ newapi/DB_bots/pymysql_bot.py
@@ -44,6 +44,7 @@ def sql_connect_pymysql(
             results = cursor.fetchall()
         except Exception as e:
             logger.exception(e)
+            logger.exception('Exception during fetchall', exc_info=True)
             return []
 
     return results
--- newapi/api_utils/except_err.py
+++ newapi/api_utils/except_err.py
@@ -1,5 +1,4 @@
 """
-except_err|warn_err|exception_err
 """
 
 import inspect
